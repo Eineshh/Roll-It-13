8eec793d9aae614f0cd7405385fa1028e8bb6444
Eineshh <142182572+Eineshh@users.noreply.github.com>
2024-02-12 14:56:42 +1300

    Add files via upload

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -1,16 +1,51 @@
-print("🎲🎲 Roll it 13 🎲🎲")
+# checks users enter yes (y) or no (n)
+def yes_no(question):
+    while True:
+        print()
+        response = input(question).lower()
 
+        # checks user response, question
+        # repeats if users don't enter yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter Yes / No")
+
+
+def instruction():
+    print('''
+    
+✦✦✦ Instructions ✦✦✦
+
+First off, you choose a scoring objective (e.g. The first player to score 50 points or more). 
+
+Next, you roll a die to determine how many points you earn for each round. 
+
+The player who comes closest to 13 wins the round. 
+
+If you roll a double on your first roll, you obtain double the amount of points (6 X 2 = 12). If you win the round, 
+your score will grow by the number you earned
+
+You receive zero points if you lose the round. 
+
+In a tie, the number of bonus points awarded to each player is the same.  
+
+
+    ''')
+
+
+# Main routine
+print()
+print("🎲🎲 Roll it 13 🎲🎲")
 
 # loop for testing purposes
 
-while True:
-    print()
-    want_instructions = input("Do you want to read the instructions?: ").lower()
+want_instructions = yes_no("Do you want to read the instructions?: ")
 
-    # checks users enter yes (y) or no (n)
-    if want_instructions == "yes" or want_instructions == "y":
-        print("You chose yes")
-    elif want_instructions == "no" or want_instructions == "n":
-        print("You chose no")
-    else:
-        print("You did not choose a valid response")
+# checks users enter yes (y) or no (n)
+if want_instructions == "yes":
+    instruction()
+
+print("program continues")
