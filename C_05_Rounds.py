519e4d4c90ba4a3278718a8ee630f27bbb535249
Eineshh <142182572+Eineshh@users.noreply.github.com>
2024-02-19 13:10:55 +1300

    Fixed over 13 Issue

--- C_05_Rounds.py
+++ C_05_Rounds.py
@@ -47,6 +47,7 @@ else:
 
 # output initial move results
 print(f"You rolled a total of {user_points},  {double_feedback}")
+print()
 
 # Get initial dice rolls for computer
 computer_first = two_rolls()
@@ -55,11 +56,43 @@ computer_points = computer_first[0]
 print(f"The computer rolled a total of {computer_points}.")
 
 # Loop (while both user / computer have <= 13 points)...
-# ask user if they want to roll again, update
-# points / status
+while computer_points < 13 and user_points < 13:
+    # ask user if they want to roll again, update
+    # points / status
+    print()
+    roll_again = input("Do you want to roll the dice (type 'no' to pass): ")
+    if roll_again == "yes":
+        user_move = roll_die()
+        user_points += user_move
+        print(f"You rolled a {user_move}.  You now have {user_points} points.")
 
-# Roll die for computer and update computer points
+    print("\nPress <enter> to continue...")
+    input()
+
+    # Roll die for computer and update computer points
+    computer_move = roll_die()
+    computer_points += computer_move
+    print(f"The computer rolled a {computer_move}.  The computer"
+          f" now has {computer_points}.")
+
+    print()
+    if user_points > computer_points:
+        result = "You are ahead!"
+    else:
+        result = "The computer is ahead!"
+
+    print(f"⟡⟡⟡ Round Update ⟡⟡⟡: {result} ")
+    print(f"User Score: {user_points} \t | \t Computer Score: {computer_points}")
 
 # Outside loop - doubler user points if they won and are eligible
 
 # Show rounds result
+if user_points < computer_points:
+    print("Sorry - You lost this round and no points "
+          "were added to your total score. The computer's score has "
+          f"increased by {computer_points} points.")
+
+# currently does not include double points!
+else:
+    print(f"Yay! You won the round and {user_points} points have "
+          f"been added to your score")
